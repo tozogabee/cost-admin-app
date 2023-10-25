49176016b56a03e9f944cdaf82283f0998b1bf7a
Gabor Toth <tozogabee@gmail.com>
2023-10-25 12:20:39 +0200

    Initial Spring Boot commit.

---
+++ src/test/java/cost/admin/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package cost.admin.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
